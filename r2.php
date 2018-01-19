f59b38a5c43e23a934dbe1c1a1277a81d3737535
YuzukiMIMURA <admin@MacBook-Pro.local>
2018-01-19 15:51:35 +0900

    swiftから配列を受け取れるように変更

--- r2.php
+++ r2.php
@@ -1,18 +1,20 @@
 <?php
-try {
+try { // try-catchでエラー取得
+
+
 $pdo = new PDO('mysql:dbname=url;host=localhost;charset=utf8','root','',
 array(PDO::ATTR_EMULATE_PREPARES => false));
 
-$id = 1111;
-$url = "test";
+$id = $_POST["id"];     //swiftから送られてくるディクショナリー型変数を取得　配列中のStringがKey
+$url = $_POST["url"];
 
 $stmt = $pdo -> prepare("INSERT INTO test(id,url) VALUES (:id,:url)");
 
 
-$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+$stmt->bindParam(':id', $id, PDO::PARAM_STR);
 $stmt->bindValue(':url', $url, PDO::PARAM_STR);
 
-$stmt -> execute();
+$stmt -> execute(); //INSERT実行
 
 
 } catch (PDOException $e) {
