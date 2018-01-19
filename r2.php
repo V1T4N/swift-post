52adb672c8ee10d22cc5cbe58109e7098e647636
YuzukiMIMURA <admin@MacBook-Pro.local>
2018-01-20 00:43:59 +0900

    IDとパス機能追加

--- r2.php
+++ r2.php
@@ -5,7 +5,7 @@ try { // try-catchでエラー取得
 $pdo = new PDO('mysql:dbname=url;host=localhost;charset=utf8','root','',
 array(PDO::ATTR_EMULATE_PREPARES => false));
 
-$id1 = $_POST["id"];        //swiftから送られてくるディクショナリー型変数を取得　配列中のStringがKey
+$id1 = $_POST["id"];        //swiftから送られてくるディクショナリー型変数を取得　配列中のStringがKey 
 $pass1 = $_POST["pass"];
 $url1 = $_POST["url"];
 
